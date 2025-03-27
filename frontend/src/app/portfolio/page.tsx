72804d0c008f286d1d9cae78a9f8b82e0807c385
Akdrewp <jamesterpaul@gmail.com>
2025-03-27 08:19:49 -0400

    SAGE-005 Added JSdoc and Todo comment
    Added JSdoc to createPortolio element
    Added TODO comment to Page.tsx

--- frontend/src/app/portfolio/page.tsx
+++ frontend/src/app/portfolio/page.tsx
@@ -5,6 +5,14 @@ import Portfolio from "@/components/portfolio/portfolio-view"
 import CreatePortfolio from "@/components/portfolio/create-portfolio"
 
 export default function PortfolioView() { //TODO Rename this function
+
+  /**
+   * TODO
+   * Ideally this is always shown when navigating to /portfolio
+   * Plan is to delegate conditional rendering based on
+   * selected portfolio to Portfolio element
+   */
+
   return (
     <PageLoader fallback={
       <CreatePortfolio/>
