e652ce6b959bb9f1d1fdf2c57deb30dc1537afb9
Akdrewp <jamesterpaul@gmail.com>
2025-03-28 10:05:20 -0400

    SAGE-007 Added StockPortfolio
    Added stock portfolio view

--- frontend/src/components/portfolio/portfolio-view.tsx
+++ frontend/src/components/portfolio/portfolio-view.tsx
@@ -1,9 +1,9 @@
-
+import StockPortfolio from "./portfolio";
 
 export default function Portfolio() {
     return (
         <div>
             Portfolio placeholder
         </div>
-    )
+    );
 }
