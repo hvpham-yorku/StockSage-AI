38bb2b550c30a6fe33f2c4b7d0ddc93f0976cead
Akdrewp <jamesterpaul@gmail.com>
2025-03-30 17:40:52 -0400

    SAGE-007 Added chart data points interface
    Added chart interface to portfolio_perfomance_values.ts
    for the future implementation of the datapoint getter for the graph

--- frontend/src/lib/portfolio/portfolio_perfomance_value.ts
+++ frontend/src/lib/portfolio/portfolio_perfomance_value.ts
@@ -3,6 +3,30 @@ interface DataPoint {
     value: number
 }
 
+
+
+/**
+ * @todo Rename interface
+ * @todo Rename methods
+ * Interface for perfomance charts graph.
+ * Given dates gets datapoints appropriate to timeline.
+ * Something like this should be made to get data points
+ * at least until backend is made this will do
+ */
 interface chart {
-    
+    portfolioId: string
+
+    /**
+     * 
+     * @param days Amount of days worth of data points requested
+     * @returns {DataPoint[]} Array of data points starting from most recent
+     */
+    getDataPoints(days: number) : DataPoint[]
+
+    /**
+     * 
+     * @param date Date of datapoint requested
+     * @returns {DataPoint} Data point for specified day
+     */
+    getDataPoint(date: string) : DataPoint[]
 }
